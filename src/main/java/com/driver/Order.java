0b17b166c4500d8543d3a17aac58b8cb4cd313c3
Aaditya kandu <aadityakandu@gamil.com>
2023-02-11 17:55:39 +0530

    First Solution

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,11 +9,24 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+
+        this.id = id;
+        this.deliveryTime = deliveryTime;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
 }
